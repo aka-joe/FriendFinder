a153f5c6907de6062acf527e2a7b572e90a6abe3
Joseph Ahn <joseph@ahnga.net>
2019-02-07 13:36:50 -0800

    Added some backend JS

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,17 @@
+// LOAD DATA
+var friendsData = require("../data/friends");
+
+// ROUTING
+module.exports = function(app) {
+  
+    // API GET Request
+  app.get("/api/friends", function(req, res) {
+    res.json(friendsData);
+  });
+  
+  // API POST Requests
+  app.post("/api/friends", function(req, res) {
+    friendsData.push(req.body);
+    res.json(req.body);
+  });
+};
